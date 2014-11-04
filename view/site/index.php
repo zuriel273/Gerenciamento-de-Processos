3a1955d9aed7decea07493a09ca38d90c770c358
= <=>
2014-11-04 14:18:32 -0300

    Texto inicial

--- view/site/index.php
+++ view/site/index.php
@@ -1,4 +1,9 @@
 <h1>BEM VINDO</h1>
 <div class="well">
-	O SGP ...	
+	 <p>O SGP visa auxiliar no fluxo dos processos da Universiade federal da Bahia,</p> 
+	 <p>ajudando a eliminar/acelerar algumas etapas, dando suporte aos funcionários encarregados 
+	 das tramitações,</p><p> e também ao colegiado na gestão desses processos. 
+	 Além dessas atividades, o software deverá apresentar relatórios com dados estatísticos 
+	 das solicitações e pareceres.</p>
+	
 </div>
